e340583940bffd9997d52f253af48e8a352f6b4d
Kamrul Hassan <123955650+kamrul1250@users.noreply.github.com>
2025-07-25 06:08:45 +0600

    Add files via upload

---
+++ Calculator-main/Calculator-main/Web Lab Calculator-20250624T134428Z-1-001/Web Lab Calculator/script.js
@@ -0,0 +1,21 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  if (display.value === "0") {
+    display.value = input;
+  } else {
+    display.value += input;
+  }
+}
+
+function clearDisplay() {
+  display.value = "0";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
